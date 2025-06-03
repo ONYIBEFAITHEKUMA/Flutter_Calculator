229c27ae83c7ca536023c6899e03804c6e85827d
ONYIBEFAITHEKUMA <ONYIBEFAITHEKUMA@gmail.com>
2025-06-03 10:56:37 -0700

    added some functionality

--- lib/calculotor.dart
+++ lib/calculotor.dart
@@ -1,20 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
-class CalculotorAPP extends StatefulWidget {
-  const CalculotorAPP({super.key});
-
+class CalculatorAPP extends StatefulWidget {
   @override
-  State<CalculotorAPP> createState() => _CalculotorAPPState();
+  State<CalculatorAPP> createState() => _CalculatorState();
 }
 
-class _CalculotorAPPState extends State<CalculotorAPP> {
+class _CalculatorState extends State<CalculatorAPP> {
+  String input = '';
+  String result = '';
+
+  void buttonPressed(String value) {
+    setState(() {
+      if (value == 'C') {
+        input = '';
+        result = '';
+      } else if (value == '=') {
+        try {
+          Parser p = Parser();
+          Expression exp = p.parse(input);
+          ContextModel cm = ContextModel();
+          double eval = exp.evaluate(EvaluationType.REAL, cm);
+          result = eval.toString();
+        } catch (e) {
+          result = 'Error';
+        }
+      } else {
+        input += value;
+      }
+    });
+  }
+
+  Widget buildButton(String value, {Color color = Colors.blue}) {
+    return SizedBox(
+      width: 70,
+      height: 70,
+      child: ElevatedButton(
+        onPressed: () => buttonPressed(value),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: color,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        ),
+        child: Text(value, style: const TextStyle(fontSize: 24)),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // backgroundColor: Colors.black,
-        title: Text('Calculator App'),
+      appBar: AppBar(title: const Text('Calculator')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text(input, style: const TextStyle(fontSize: 32)),
+          Text(result, style: const TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
+          Wrap(
+            spacing: 10,
+            runSpacing: 10,
+            children: [
+              ...['7', '8', '9', '/',
+                  '4', '5', '6', '*',
+                  '1', '2', '3', '-',
+                  '0', '.', '=', '+',
+                  'C'].map((e) => buildButton(e, color: _isOperator(e) ? Colors.orange : Colors.blue))
+            ],
+          ),
+        ],
       ),
     );
+  }
+
+  bool _isOperator(String value) {
+    return ['/', '*', '-', '+', '='].contains(value);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'calculotor.dart';
 
 void main(List<String> args) {
@@ -11,7 +12,7 @@ class Calculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: CalculotorAPP(),
+      home: CalculatorAPP(),
       debugShowCheckedModeBanner: false,
     );
   }
